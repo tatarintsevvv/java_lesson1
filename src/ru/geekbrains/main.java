cef0da749c8d7951dade36c8e490cc32fac4a22d
tatarintsevvv <tatarintsev_vv@mail.ru>
2020-12-01 22:21:08 +0300

    задание ко второму уроку на Java Pro

--- src/ru/geekbrains/main.java
+++ src/ru/geekbrains/main.java
@@ -2,85 +2,69 @@ package ru.geekbrains;
 
 import java.util.*;
 
+// 1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4, при подаче
+// массива другого размера необходимо бросить исключение MyArraySizeException.
+// 2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать.
+// Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или
+// текст вместо числа), должно быть брошено исключение MyArrayDataException – с детализацией, в какой
+// именно ячейке лежат неверные данные.
+// 3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException
+// и MyArrayDataException и вывести результат расчета.
+class MyArraySizeException extends Exception {
+}
+
+class MyArrayDataException extends Exception {
+    private int row;
+    private int column;
+    public MyArrayDataException(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return "Не число в " + row + " строке и " + column + " столбце";
+    }
+}
 class Main {
     public static void main(String args[]) {
-        long nanoStartTime, nanoEndTime;
-
-        //  выполняет задачу 1.1
-        LinkedList<Object> list = new LinkedList<>();
-        list.add((Object)new Human());
-        list.add((Object)new Robot());
-        list.add((Object)new Cat());
-
-        Iterator iterator = list.iterator();
-        while(iterator.hasNext()) {
-            Object obj = iterator.next();
-            if(obj instanceof Human) {
-                ((Human)obj).canRun();
-            } else if(obj instanceof Cat) {
-                ((Cat)obj).canRun();
-            } else if(obj instanceof Robot) {
-                ((Robot)obj).canRun();
-            }
+        final String[][] arr = {
+                {"1", "1", "1", "1"},
+                {"2", "2", "c", "2"},
+                {"3", "3", "3", "3"},
+                {"4", "4", "4", "4"}
+        };
+        try {
+            task1(arr);
+        } catch(MyArraySizeException e) {
+            System.out.println("Array has not 4x4 dimensions");
+        } catch(MyArrayDataException e) {
+            System.out.println(e.toString());
         }
-
-        iterator = list.iterator();
-        while(iterator.hasNext()) {
-            Object obj = iterator.next();
-            if(obj instanceof Human) {
-                ((Human)obj).canJump();
-            } else if(obj instanceof Cat) {
-                ((Cat)obj).canJump();
-            } else if(obj instanceof Robot) {
-                ((Robot)obj).canJump();
-            }
-        }
-
-        // задача 1.2, реализованы соответствующие классы
-        Wall wall = new Wall(0.7);
-        TreadMill treadmill = new TreadMill(700);
-
-
-        // задача 1.3 и сразу же 1.4, препятствия я сделал массивом, а участников
-        // контейнером (двусвязным списком)
-        Object[] arr = { (Object)wall, (Object)treadmill};
-        for (Object obj: arr) {
-            if(obj instanceof Wall) {
-                Wall objWall = (Wall)obj;
-                iterator = list.iterator();
-                while(iterator.hasNext()) {
-                    boolean result = ((Wall)objWall).jump(iterator.next());
-                    if(!result) {
-                        iterator.remove();
-                    }
-                }
-            } else if(obj instanceof TreadMill) {
-                TreadMill objTreadmill = (TreadMill)obj;
-                iterator = list.iterator();
-                while(iterator.hasNext()) {
-                    boolean result = objTreadmill.run(iterator.next());
-                    if(!result) {
-                        iterator.remove();
-                    }
-                }
-            } else {
-                System.out.println("Неизвестное препятствие!");
-            }
-        }
-
     }
 
     // метод выполняетзадачу 1.4
-    public static void task4() {
+    public static void task1(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        if(arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch(NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        System.out.println("Сумма = " + sum);
     }
 
 
-    // метод выполняет задачу 1.3
-    public static void task3() {
-    }
-
-
-    // заджание 3.5, сортировка методом выбора
-    public static void task5() {
-    }
 }
