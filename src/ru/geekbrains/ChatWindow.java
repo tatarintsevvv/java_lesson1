d4dd3dea488061b1ca3bd196ba57fad92f1d9ada
tatarintsevvv <tatarintsev_vv@mail.ru>
2020-12-05 18:10:17 +0300

    задача к 4 уроку pro, окно чата

---
+++ src/ru/geekbrains/ChatWindow.java
@@ -0,0 +1,77 @@
+package ru.geekbrains;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class ChatWindow extends JFrame {
+    public ChatWindow() throws HeadlessException {
+        super("Окно чата");
+        setBounds(300, 300, 500, 400);
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Панель содержимого
+        Container container = getContentPane();
+
+        SpringLayout layout = new SpringLayout();
+        container.setLayout(layout);
+
+        // панель отображения чата
+//        JTextPane chat = new JPane("Многострочное поле");
+//        JLabel chat = new JLabel("Многострочное поле");
+        ChatPanel chat = new ChatPanel();
+        JScrollPane scrollPane = new JScrollPane(chat);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        container.add(scrollPane);
+
+
+        JTextField textInput = new JTextField();
+        container.add(textInput);
+        JButton submit = new JButton("Отправить");
+        submit.addActionListener(e -> {
+            String text = textInput.getText();
+            if(!text.isEmpty() && !text.isBlank()) {
+                chat.addString(text);
+                chat.repaint();
+                /*
+                Dimension dim = chat.getPreferredSize();
+                Dimension realDim = chat.getSize();
+                if((realDim.height > dim.height) || (realDim.width > dim.width)) {
+                    dim.height = Math.max(realDim.height, dim.height);
+                    dim.width = Math.max(realDim.width, dim.width);
+                    chat.setPreferredSize(dim);
+                    chat.revalidate();
+                    scrollPane.repaint();
+                }
+*/
+//                scrollPane.repaint();
+            } else {
+                JOptionPane.showMessageDialog(
+                        container,
+                        "Не введен текст",
+                        "Ошибка",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+            textInput.setText("");
+            textInput.requestFocus();
+        });
+        container.add(submit);
+
+        layout.putConstraint(SpringLayout.SOUTH, submit , -10, SpringLayout.SOUTH,  container);
+        layout.putConstraint(SpringLayout.EAST, submit, -10, SpringLayout.EAST, container);
+        layout.putConstraint(SpringLayout.NORTH, textInput, 2, SpringLayout.NORTH, submit);
+        layout.putConstraint(SpringLayout.EAST, textInput, -10, SpringLayout.WEST, submit);
+        layout.putConstraint(SpringLayout.WEST, textInput, 20, SpringLayout.WEST, container);
+        layout.putConstraint(SpringLayout.SOUTH, scrollPane, -5, SpringLayout.NORTH, submit);
+        layout.putConstraint(SpringLayout.NORTH, scrollPane, 5, SpringLayout.NORTH, container);
+        layout.putConstraint(SpringLayout.EAST, scrollPane, -5, SpringLayout.EAST, container);
+        layout.putConstraint(SpringLayout.WEST, scrollPane, 5, SpringLayout.WEST, container);
+
+
+        setVisible(true);
+
+    }
+}
--- src/ru/geekbrains/main.java
+++ src/ru/geekbrains/main.java
@@ -1,5 +1,6 @@
 package ru.geekbrains;
 
+import javax.swing.*;
 import java.util.*;
 
 // 1. Создать массив с набором слов (10-20 слов, среди которых должны встречаться повторяющиеся). Найти и вывести
@@ -12,62 +13,6 @@ import java.util.*;
 
 class Main {
     public static void main(String args[]) {
-        final String[] arr = {
-                "Robot1",
-                "Robot2",
-                "Robot3",
-                "Robot4",
-                "Robot5",
-                "Robot1",
-                "Robot6",
-                "Robot1",
-                "Robot7",
-                "Robot3",
-                "Robot8",
-                "Robot9",
-                "Robot10",
-                "Robot7",
-                "Robot11",
-        };
-        HashSet<String> set = new HashSet<>();
-        for (String str: arr) {
-            set.add(str);
-        }
-        Iterator iterator = set.iterator();
-        while(iterator.hasNext()) {
-            String temp = (String)iterator.next();
-            int count = 0;
-            for (int i = 0; i < arr.length; i++) {
-                if(arr[i].equals(temp)) {
-                    count++;
-                }
-            }
-            System.out.println(temp + " встречается " + count + " раз");
-        }
-
-        // теперь задание 3.2
-        PhoneBook book = new PhoneBook();
-        book.add("Robot", "+7(911)111-11-11");
-        book.add("Cat", "+7(911)111-11-12");
-        book.add("Wilson", "+7(911)111-11-13");
-        book.add("Wall", "+7(911)111-11-14");
-        book.add("Nelson", "+7(911)111-11-15");
-        book.add("Treadmill", "+7(911)111-11-16");
-        book.add("Human", "+7(911)111-11-17");
-        book.add("Wilson", "+7(911)111-11-18");
-        book.add("Nelson", "+7(911)111-11-19");
-        book.add("Brown", "+7(911)111-11-21");
-
-        String lastName = "Wilson";
-        ArrayList<String> list = book.get(lastName);
-        if(list.isEmpty()) {
-            System.out.println(lastName + " without phone");
-        } else {
-            System.out.println(lastName + " with phone(s):");
-            iterator = list.iterator();
-            while(iterator.hasNext()) {
-                System.out.println(iterator.next());
-            }
-        }
+        ChatWindow chat = new ChatWindow();
     }
 }
